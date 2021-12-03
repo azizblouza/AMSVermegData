5715626e5846b549c633518f51fd20563de81460
azizblouza <aziz.blouza@rades.r-iset.tn>
2021-12-03 15:01:45 +0100

    first commit

---
+++ src/main/java/com/sip/ams/Controllers/ProviderController.java
@@ -0,0 +1,59 @@
+package com.sip.ams.Controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sip.ams.entities.Provider;
+import com.sip.ams.repositories.ProviderRepository;
+
+@Controller
+@RequestMapping("/provider/")
+public class ProviderController {
+	@Autowired
+	ProviderRepository providerRepository;
+
+	@GetMapping("list")
+	public String listProviders(Model model) {
+		System.out.println(providerRepository.findAll());
+		model.addAttribute("providers", providerRepository.findAll());
+		return "provider/ListProvider";
+	}
+
+	@GetMapping("add")
+	public String showAddProviderForm(Model model) {
+		Provider provider = new Provider();// object dont la valeur desattributs par defaut
+		model.addAttribute("provider", provider);
+		return "provider/addProvider";
+	}
+
+	@PostMapping("add")
+	public String addProvider(@Valid Provider provider, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "provider/addProvider";
+		}
+		providerRepository.save(provider);
+		return "redirect:list";
+	}
+	@GetMapping("delete/{id}")
+	public String deleteProvider(@PathVariable("id") long id, Model model) {
+
+	//long id2 = 100L;
+	Provider provider = providerRepository.findById(id)
+	.orElseThrow(()-> new IllegalArgumentException("Invalid provider Id:" + id));
+	System.out.println("suite du programme...");
+	providerRepository.delete(provider);
+	/*model.addAttribute("providers",
+	providerRepository.findAll());
+	return "provider/listProviders";*/
+	return "redirect:../list";
+	}
+
+}
